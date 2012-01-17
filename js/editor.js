b34b017efdaf1db31bfce83028e4ed8c892babff
Alex <Alex@localhost>
2012-01-17 13:48:53 -0700

    Work on the editor. Right now it adds resolution information to the document.

--- js/editor.js
+++ js/editor.js
@@ -2,6 +2,27 @@ function Edit () {
 	
 }
 
+var size = 150;
+
 function processFiles (evt) {
-	console.log(evt.files);
+	var files = evt.target.files;
+	var path = '';
+	
+	for (var f in files) {
+		if (path = files[f].fileName) {
+			var img = new Image();
+			img.src = 'images/' + path;
+			img.name = path;
+			img.onload = function (path) {
+				var p = $('<p>');
+				var sp = $('<span>')
+				sp.attr('width', this.width);
+				sp.attr('height', this.height);
+				sp.addClass('file');
+				sp.text(this.name);
+				p.append(sp);
+				$('#data').append(p);
+			}
+		}
+	}
 }
